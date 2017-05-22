d7dea4b359205296657dd1d054a36baa1d0733fe
deepakarchinnova <deepak.dinakaran@archinnova.com>
2017-05-22 10:33:29 +0400

    Update DemoApplication.java

--- src/main/java/com/arc/DemoApplication.java
+++ src/main/java/com/arc/DemoApplication.java
@@ -13,7 +13,7 @@ public class DemoApplication extends SpringBootServletInitializer {
 	@RequestMapping("/")
 	public String home(){
 
-		return "Hello World from Archinnova - from Archinnova- May"
+		return "Hello World from Archinnova - from Archinnova- May";
 			
 
 	}
