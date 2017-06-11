7e808b3da349706bc6cf85237c976ba8ec7dd7a8
Deepak Dinakaran <deepak.dinakaran@archinnova.com>
2017-06-11 20:51:32 +0400

    changed message

--- src/main/java/com/arc/DemoApplication.java
+++ src/main/java/com/arc/DemoApplication.java
@@ -13,7 +13,7 @@ public class DemoApplication extends SpringBootServletInitializer {
 	@RequestMapping("/")
 	public String home(){
 
-		return "Hello World";
+		return "Hello World - Feature A";
 		
 			
 
